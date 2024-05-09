7ef2794f508fdf0c14785abdd2d6aa13007aa2d1
Dahn12 <dadesign0709@gmail.com>
2024-05-09 17:19:13 +0900

    feat: navbar, controller 컴포넌트 틀 완성

---
+++ next-netflix/app/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import { NavItemsType } from '../../types' 
+import Image from 'next/image';
+import Home from '../../public/icons/home.svg'
+import Search from '../../public/icons/search.svg'
+import CommingSoon from '../../public/icons/comming-soon.svg'
+import Download from '../../public/icons/download.svg'
+import Hamburger from '../../public/icons/hamburger.svg'
+
+
+function Navbar() {
+const NavItems:NavItemsType[] = [
+    {
+        id : 1,
+        src : Home,
+        alt : 'home',
+        title : "Home",
+        link : "/"
+    },
+    {
+        id : 2,
+        src : Search,
+        alt : 'search',
+        title : "Search",
+        link : "/search"
+    },
+    {
+        id : 3,
+        src : CommingSoon,
+        alt : 'comming-soon',
+        title : "CommingSoon",
+        link : "/commingSoon"
+    },
+    {
+        id : 4,
+        src : Download,
+        alt : 'download',
+        title : "Download",
+        link : "/"
+    },
+    {
+        id : 5,
+        src : Hamburger,
+        alt : 'menu',
+        title : "Menu",
+        link : "/"
+    }
+]
+
+  return (
+    <div className="flex-row justify-center items-center">
+        {NavItems.map(({ id, title, src, alt, link }) => (
+                <div key={id}>
+                    <Image
+                     src={src}
+                     alt={alt}
+                     width={24}
+                     height={24}/>
+                    <span>{title}</span>
+                </div>
+            ))}
+    </div>
+  )
+}
+
+export default Navbar
--- next-netflix/app/main/page.tsx
+++ next-netflix/app/main/page.tsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Controller from "../components/Controller";
 
 
 export default function Main() {
   return (
     <div>
       <Header/>
+      <Controller/>
+      <Navbar/>
     </div>
   );
 }
