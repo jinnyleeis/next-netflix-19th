7a3b9f94bcc83c77beb526b18ed58fcea74ca591
Dahn12 <dadesign0709@gmail.com>
2024-05-10 19:37:32 +0900

    fix : carousel 이미지 비율 맞게 채워지도록 수정

--- next-netflix/app/components/Carousel.tsx
+++ next-netflix/app/components/Carousel.tsx
@@ -10,29 +10,29 @@ export const Carousel: React.FC<CarouselProps> = ({ movies }) => {
   const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
 
   useEffect(() => {
-    // 일정 시간마다 currentMovieIndex를 업데이트하여 다음 영화 이미지를 보여줌
     const interval = setInterval(() => {
       setCurrentMovieIndex((prevIndex) =>
         prevIndex === movies.length - 1 ? 0 : prevIndex + 1
       );
-    }, 5000); // 5초마다 변경, 변경하려는 시간 간격에 맞게 조정 가능
+    }, 5000); 
 
     // 컴포넌트가 언마운트되면 interval을 정리
     return () => clearInterval(interval);
-  }, [movies.length]); // movies 배열의 길이가 변경될 때마다 useEffect가 호출되도록 설정
+  }, [movies.length]); // movies 배열의 길이가 변경될 때마다 useEffect가 호출
 
   return (
     <div className="w-[375px] h-[415px] relative">
-      {movies.length > 0 && (
-        <Image
-          src={`https://image.tmdb.org/t/p/original${movies[currentMovieIndex].backdrop_path}`}
-          alt={movies[currentMovieIndex].title}
-          width={375}
-          height={415}
-          layout="responsive"
-          priority
-        />
-      )}
+      <div className='size-full'>
+        {movies.length > 0 && (
+          <Image
+            src={`https://image.tmdb.org/t/p/original${movies[currentMovieIndex].backdrop_path}`}
+            alt={movies[currentMovieIndex].title}
+            layout="fill" 
+            objectFit="cover" // 이미지가 잘리지 않고 채워지도록
+            priority
+          />
+        )}
+      </div>
     </div>
   );
 };
