48a7c000ad174022d15855b54468ff99d06b1165
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-05-15 21:20:02 +0900

    feat : current path에 따른, navitem 텍스트 색상 변화

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export interface NavItemsType {
-    id: number;
-    src: string;
-    alt:string;
-    title: string;
-    link: string;
+  id: number;
+  src: string;
+  alt: string;
+  title: string;
+  link: string;
 }
