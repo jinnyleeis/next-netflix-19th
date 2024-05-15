2573a61b5577ab167acf49838d996571473b5867
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-05-16 00:36:44 +0900

    feat. : id에 따른 movie detail 받아오는 api 구현, delete : 불필요 img

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,12 +1,20 @@
 // ./app/detail/[id]/page.tsx
 
+import { getMovieDetailsById } from '../../utils/movieAPI';
+import { Movie } from '../../types/movies';
+
+
 interface DetailProps {
   params: { id: string };
 }
-function Detail({ params }: DetailProps) {
+
+export async function Detail({ params }: DetailProps) {
+  const movieDetail: Movie = await getMovieDetailsById(params.id);
+  //ex) interstella : http://localhost:3000/detail/157336
   return (
     <div>
       <h1>Movie ID: {params.id}</h1>
+      <h1>{movieDetail.title}</h1>
       {/* ID에 따른 정보 렌더링 */}
     </div>
   );
