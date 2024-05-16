3cd72e9e616e83be94d2b22bd9178a5e0b88fe56
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-05-16 23:11:06 +0900

    feat : api 1개로, main 페이지 서버 컴포넌트로 변경
    utils -> movieAPI에 fetch 함수
    이를 main 페이지에서 이용해서, 영화 정보 불러오기
    클라이언트 컴포넌트인 search 페이지에서 api를 통해, 검색어에 따른 영화정보 불러올 수 있도록 구현

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,71 +1,43 @@
-'use client';
-
 import Header from '../components/Header';
 import Controller from '../components/Controller';
 import Carousel from '../components/Carousel';
 import MovieList from '../components/MainLists';
-import { useEffect, useState } from 'react';
 import { Movie } from '../types/movies';
+import {fetchAllCategories } from '../utils/movieAPI';
 
-export default function Main() {
-  const [movies, setMovies] = useState<Movie[][]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+
+export default async function Main() {
   const mainListsTitle = [
     'Previews',
     'New Releases',
     'Popular on Netflix ',
-    'Top 10 in Korea Today'
+    'Top 10 in Korea Today',
   ];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const apis = ['/api1', '/api2', '/api3', '/api4'];
-        const responses = await Promise.all(apis.map((api) => fetch(api)));
-        const data = await Promise.all(
-          responses.map((response) => {
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-          }),
-        );
-        setMovies(data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Failed to load movies:', error);
-        setError('Failed to load movies.');
-        setLoading(false);
-      }
-    };
+  try {
+    const movies = await fetchAllCategories(); 
 
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="h-[1070px] w-[375px] overflow-y-auto ">
+        <Header />
+        <Carousel movies={movies[0]} />
+        <Controller />
+        {movies.map((movieList, index) => (
+          <div key={index}>
+            <h2 className="ml-[16px] mt-[22px] text-[20.92px] font-bold">
+              {mainListsTitle[index]}
+            </h2>
+            <MovieList
+              movies={movieList}
+              itemClass={index === 0 ? 'rounded-full' : ''}
+              isCircular={index === 0}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  } catch (error) {
+    return <div>Error</div>;
   }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  return (
-    <div className="h-[768px] w-[375px] overflow-y-auto ">
-      <Header />
-      <Carousel movies={movies[0]} />
-      <Controller />
-      {movies.map((movieList, index) => (
-        <div key={index}>
-          <h2 className="ml-[16px] mt-[22px] text-[20.92px] font-bold">{mainListsTitle[index]}</h2>
-          <MovieList
-            movies={movieList}
-            itemClass={index === 0 ? 'rounded-full' : ''}
-            isCircular={index === 0}
-          />
-        </div>
-      ))}
-    </div>
-  );
 }
+
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,97 +1,61 @@
 'use client';
 import { Movie } from '@/app/types/movies';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
 import searchIcon from '@/public/icons/search.svg';
 import deleteIcon from '@/public/icons/delete.svg';
 import SearchLists from '../components/SearchLists';
 
-
-function Search() {
-const [inputValue, setInputValue] = useState("");
-const [movies, setMovies] = useState<Movie[][]>([]);
-const [loading, setLoading] = useState(true);
-const [error, setError] = useState('');
-
+export default function Search() {
+  const [movies, setMovies] = useState<Movie[][]>([]);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const apis = ['/api1', '/api2', '/api3', '/api4'];
-        const responses = await Promise.all(apis.map((api) => fetch(api)));
-        const data = await Promise.all(
-          responses.map((response) => {
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-          }),
-        );
+        const response = await fetch('/api');
+        const data = await response.json();
         setMovies(data);
-        setLoading(false);
       } catch (error) {
         console.error('Failed to load movies:', error);
-        setError('Failed to load movies.');
-        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  console.log(movies);
-
-  return(
-  <div>
+  return (
     <div>
-      <Image
-        src={searchIcon}
-        alt= 'search'
-        width={20}
-        height={20}
-      />
-     <input
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Search for a show, movie, genre, e.t.c."
-      />
-      <Image
-        src={deleteIcon}
-        alt= 'delete'
-        width={20}
-        height={20}
-        onClick={() => setInputValue("")}
-      />
+      <div>
+        <Image src={searchIcon} alt="search" width={20} height={20} />
+
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Search for a show, movie, genre, e.t.c."
+        />
+        <Image
+          src={deleteIcon}
+          alt="delete"
+          width={20}
+          height={20}
+          onClick={() => setInputValue('')}
+        />
+      </div>
+      <div>Top Searches</div>
+
+      {inputValue.trim() === ''
+        ? movies.map((movieList, index) => (
+            <div key={index}>
+              {movieList.map((movie, movieIndex) => (
+                <SearchLists key={movieIndex} movie={movie} />
+              ))}
+            </div>
+          ))
+        : movies
+            .flat()
+            .filter((movie) => movie.title.toLowerCase().includes(inputValue.toLowerCase()))
+            .map((filteredMovie, index) => <SearchLists key={index} movie={filteredMovie} />)}
     </div>
-    <div>Top Searches</div>
-
-    {inputValue.trim() === '' ? (
-      movies.map((movieList, index) => (
-        <div key={index}>
-          {movieList.map((movie, movieIndex) => (
-          <SearchLists key={movieIndex} movie={movie} />
-          ))}
-        </div>
-      ))
-      ) : 
-      (movies
-        .flat()
-        .filter(movie => movie.title.toLowerCase().includes(inputValue.toLowerCase()))
-        .map((filteredMovie, index) => (
-          <SearchLists key={index} movie={filteredMovie} />
-        ))
-      )
-    }
-  </div>
-)
+  );
 }
-
-export default Search;
