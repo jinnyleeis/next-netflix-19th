cc2a320b96dca4eaf33eb17cdffe183f7e42a245
Dahn12 <dadesign0709@gmail.com>
2024-05-17 23:22:09 +0900

    fix: navbar 색상변경

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,19 @@
 'use client';
 
-import { NavItemsType } from '../../types';
+import { NavItemsType } from '../types/types';
 import Image from 'next/image';
 import Link from 'next/link';
-import Home from '../../public/icons/home.svg';
-import Search from '../../public/icons/nav-search.svg';
-import CommingSoon from '../../public/icons/commingSoon.svg';
-import Download from '../../public/icons/download.svg';
-import Menu from '../../public/icons/menu.svg';
-import HomeIndicator from '../../public/icons/homeIndicator.svg';
+import Home from '@/public/icons/home.svg';
+import HomeClicked from '@/public/icons/homeClicked.svg';
+import Search from '@/public/icons/nav-search.svg';
+import SearchClicked from '@/public/icons/nav-searchClicked.svg';
+import CommingSoon from '@/public/icons/commingSoon.svg';
+import CommingSoonClicked from '@/public/icons/commingSoonClicked.svg';
+import Download from '@/public/icons/download.svg';
+import DownloadClicked from '@/public/icons/downloadClicked.svg';
+import Menu from '@/public/icons/menu.svg';
+import MenuClicked from '@/public/icons/menuClicked.svg';
+import HomeIndicator from '@/public/icons/homeIndicator.svg';
 import { usePathname } from 'next/navigation';
 
 function Navbar() {
@@ -16,6 +21,7 @@ function Navbar() {
     {
       id: 1,
       src: Home,
+      clicked: HomeClicked,
       alt: 'home',
       title: 'Home',
       link: '/main',
@@ -23,6 +29,7 @@ function Navbar() {
     {
       id: 2,
       src: Search,
+      clicked: SearchClicked,
       alt: 'search',
       title: 'Search',
       link: '/search',
@@ -30,6 +37,7 @@ function Navbar() {
     {
       id: 3,
       src: CommingSoon,
+      clicked: CommingSoonClicked,
       alt: 'comming-soon',
       title: 'Comming Soon',
       link: '/commingSoon',
@@ -37,6 +45,7 @@ function Navbar() {
     {
       id: 4,
       src: Download,
+      clicked: DownloadClicked,
       alt: 'download',
       title: 'Download',
       link: '/download',
@@ -44,6 +53,7 @@ function Navbar() {
     {
       id: 5,
       src: Menu,
+      clicked: MenuClicked,
       alt: 'menu',
       title: 'Menu',
       link: '/menu',
@@ -62,12 +72,12 @@ function Navbar() {
 
   return (
     <div className={`fixed bottom-0 ${navbarClass}`}>
-      <div className="mb-0 flex h-[44px] w-[385px] items-center justify-center space-x-[46px] bg-[#121212]">
-        {NavItems.map(({ id, title, src, alt, link }) => (
+      <div className="mb-0 flex h-[44px] w-[385px] items-center justify-center space-x-[40px] bg-[#121212]">
+        {NavItems.map(({ id, title, src, clicked, alt, link }) => (
           <Link href={link} key={id} className="flex flex-col items-center">
             {/* 이미지 색상은 안 변하므로 이 부분 해결 필요 */}
             <Image
-              src={src}
+              src={pathname === link ? clicked : src }
               alt={alt}
               width={20}
               height={20}
--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,6 +1,7 @@
 export interface NavItemsType {
   id: number;
   src: string;
+  clicked: string;
   alt: string;
   title: string;
   link: string;
